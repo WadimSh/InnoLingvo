d7e5f23b9769edfe3b32e3f1f5b7745b7a02c799
WadimSh <wadim11@yandex.ru>
2022-08-03 16:05:54 +0300

    feat: сделана кнопка мобильного меню

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './index.css';
 
+const mobaileMenuButton = document.querySelector('.header__button-menu');
+const mobaileCloseButton = document.querySelector('.mobile-menu__close');
+
+const popupMenu = document.querySelector('.mobile-menu');
+
+const mobaileMenu = () => {
+  popupMenu.classList.add('mobile-menu_is-open');
+}
+
+const closeMenu = () => {
+  popupMenu.classList.remove('mobile-menu_is-open');
+}
+
+mobaileMenuButton.addEventListener('click', mobaileMenu);
+mobaileCloseButton.addEventListener('click', closeMenu);
